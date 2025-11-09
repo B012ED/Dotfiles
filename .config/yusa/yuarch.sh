aaa7d85bee86f38ed5bfdb7859a71aa10eaf045b
b012ed <yusa.bored@gmail.com>
2025-11-10 01:48:54 +0800

    Update

---
+++ .config/yusa/yuarch.sh
@@ -0,0 +1,14 @@
+cat > ~/.config/waybar/config <<YUSA
+{
+  "margin-top": 10,
+  "margin-left": 10,
+  "margin-right": 10,
+  "spacing": 1,
+  "height": 49,
+  "position": "top",
+  "include": ["$HOME/.config/waybar/modules.json"],
+  "modules-left": ["group/yusa","tray","custom/clients"],
+  "modules-center": ["hyprland/workspaces"],
+  "modules-right": ["custom/swaync","group/hardware","group/myth","custom/power-button"]
+}
+YUSA
